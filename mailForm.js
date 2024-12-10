189055b4971f49c592cf05c23d2832d06dc04813
natbox <julian.kiesenhofer@outlook.at>
2024-12-10 13:53:10 +0100

    Update mailForm.js

--- mailForm.js
+++ mailForm.js
@@ -21,34 +21,3 @@ Email.send({
 }).then(
   message => alert(message)
 );
-/*
-function emailSend(){
-
-
-	var userName = document.getElementById('name').value;
-	var phone = document.getElementById('phone').value;
-	var email = document.getElementById('email').value;
-
-
-	var messageBody = "Name " + userName +
-	"<br/> Phone " + phone +
-	"<br/> Email " + email;
-	Email.send({
-    Host : "smtp.fluidmotion.pl",
-    Username : "host@fluidmotion.pl",
-    Password : "Password",
-    To : 'reviever@fluidmotion.pl',
-    From : "website@fluidmotion.pl",
-    Subject : "This is the subject",
-    Body : messageBody
-}).then(
-  message => {
-  	if(message=='OK'){
-  		swal("Secussful", "You clicked the button!", "success");
-  	}
-  	else{
-  		swal("Error", "You clicked the button!", "error");
-  	}
-  }
-);
-}*/
