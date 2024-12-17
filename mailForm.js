bdb98c147b097234eef5ca418b3679b40d85f0ab
natbox <179704619+natbox@users.noreply.github.com>
2024-12-18 00:53:14 +0100

    Update mailForm.js

--- mailForm.js
+++ mailForm.js
@@ -1,5 +1,5 @@
 var comapnyJSON={
-  CompanyName:'Fluidmotion Mechatronics UAB',
+  CompanyName:'Fluidmotion Mechatronics LLC',
   CompanyGSTIN:'37B76C238B7E1Z5',
   CompanyState:'LITHUANIA (09)',
   CompanyPAN:'B76C238B7E',
@@ -7,15 +7,15 @@ var comapnyJSON={
   CompanyAddressLine2:'Gyneju st. 16',
   CompanyAddressLine3:'Floor 5',
   PIN: '683584',
-  companyEmail:'service@fluidmotion.lt',
+  companyEmail:'service@fluidmotion.at',
   companyPhno:'+37064841505',
 };
 Email.send({
-    Host : "smtp.fluidmotion.lt",
-    Username : "proxy@fluidmotion.lt",
+    Host : "smtp.fluidmotion.at",
+    Username : "proxy@fluidmotion.at",
     Password : "password",
-    To : 'reviever@fluidmotion.lt',
-    From : "website@fluidmotion.lt",
+    To : 'reviever@fluidmotion.at',
+    From : "website@fluidmotion.at",
     Subject : document.getElementById("256").value,
     Body : document.getElementById("256").value
 }).then(
