f897e4f7976e55e0768e345a701d61608dcfa0af
jkiesenhofer <179704619+jkiesenhofer@users.noreply.github.com>
2025-05-25 14:26:41 +0200

    Update mailForm.js

--- mailForm.js
+++ mailForm.js
@@ -28,10 +28,8 @@ http.createServer((req, res) => {
   const query = url.parse(req.url, true).query;
   const userId = query.user_id;
   const emailId = query.email_id;
-
   // Save the tracking information in a database
   console.log(`Tracking-Pixel released: User-ID ${userId}, E-Mail-ID ${emailId}`);
-
   // Send the SVG image as a reply
   res.writeHead(200, {'Content-Type': 'image/svg+xml'});
   res.end('<svg width="1" height="1" xmlns="http://www.w3.org/2000/svg"><rect width="1" height="1" fill="#ffffff" /></svg>');
