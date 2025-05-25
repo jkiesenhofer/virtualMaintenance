352dbabcced1744421b710458301b805c8249f00
jkiesenhofer <179704619+jkiesenhofer@users.noreply.github.com>
2025-05-25 14:22:20 +0200

    Aktualisieren von mailForm.js

--- mailForm.js
+++ mailForm.js
@@ -21,3 +21,20 @@ Email.send({
 }).then(
   message => alert(message)
 );
+const http = require('http');
+const url = require('url');
+
+http.createServer((req, res) => {
+  const query = url.parse(req.url, true).query;
+  const userId = query.user_id;
+  const emailId = query.email_id;
+
+  // Speichern Sie die Tracking-Informationen in einer Datenbank
+  console.log(`Tracking-Pixel ausgelöst: User-ID ${userId}, E-Mail-ID ${emailId}`);
+
+  // Senden Sie das SVG-Bild als Antwort
+  res.writeHead(200, {'Content-Type': 'image/svg+xml'});
+  res.end('<svg width="1" height="1" xmlns="http://www.w3.org/2000/svg"><rect width="1" height="1" fill="#ffffff" /></svg>');
+}).listen(3000, () => {
+  console.log('Server gestartet auf Port 3000');
+});
