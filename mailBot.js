3beebe39fecf8ca65159a813cce74e07c5bc242c
jkiesenhofer <179704619+jkiesenhofer@users.noreply.github.com>
2025-06-01 14:00:59 +0200

    Aktualisieren von mailBot.js

--- mailBot.js
+++ mailBot.js
@@ -1,5 +1,5 @@
 var comapnyJSON={
-  CompanyName:'Fluidmotion Mechatronics SARL',
+  CompanyName:'Lambda Mechatronics SARL',
   CompanyGSTIN:'37B76C238B7E1Z5',
   CompanyState:'FRANCE (09)',
   CompanyPAN:'B76C238B7E',
