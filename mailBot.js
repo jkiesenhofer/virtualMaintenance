3418bc2f71bcfbe4e44ff147eb08fe0b2e5cc849
jkiesenhofer <179704619+jkiesenhofer@users.noreply.github.com>
2025-06-06 13:24:50 +0200

    Aktualisieren von mailBot.js

--- mailBot.js
+++ mailBot.js
@@ -1,5 +1,5 @@
 var comapnyJSON={
-  CompanyName:'Lambda Mechatronics B.V.',
+  CompanyName:'Lambda Consulting B.V.',
   CompanyGSTIN:'37B76C238B7E1Z5',
   CompanyState:'NETHERLANDS (09)',
   CompanyPAN:'B76C238B7E',
