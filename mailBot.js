bcde44b48991c870ac703845cb1753396311d65d
jkiesenhofer <179704619+jkiesenhofer@users.noreply.github.com>
2025-06-16 02:08:15 +0200

    Aktualisieren von mailBot.js

--- mailBot.js
+++ mailBot.js
@@ -1,6 +1,6 @@
 var comapnyJSON={
   CompanyName:'Lambda Mechatronics B.V.',
-  CompanyVAT:'ATU77591848',
+  CompanyVAT:'DE367715299',
   CompanyState:'Netherlands',
   Registrar:'1api.net', // semi-applicable agreement
   CompanyAddressLine1:'GB-Cardiff, CF10 3NS',
