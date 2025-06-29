832170834c106031367d492aee43bdb00912f36a
jkiesenhofer <179704619+jkiesenhofer@users.noreply.github.com>
2025-06-29 23:47:34 +0200

    Update mailBot.js

--- mailBot.js
+++ mailBot.js
@@ -2,8 +2,7 @@ var companyJSON={
   CompanyName:'Lambda Mechatronics B.V.',
   CompanyUID:'FN 599182 a',
   Registrar:'1api.net', // semi-applicable agreement
-  CompanyAddressLine1:'67 Hongshan Rd, Alashankou, 830092',
-  CompanyAddressLine2:'Van Leijenberghlaan 15, 1082',
+  CompanyAddressLine:'Van Leijenberghlaan 15, 1082',
   ICC-ID1:'044574499105848816',
   ICC-ID2:'898709924417112612',
   companyEmail:'service@lambda.net',
